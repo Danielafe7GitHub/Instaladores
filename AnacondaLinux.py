e5a2bd70329f3f0d8745a0f27fe42e645ac1155a
Danielafe7GitHub <daniela.milon@ucsp.edu.pe>
2018-09-15 19:10:22 -0500

    Create AnacondaLinux.py

---
+++ AnacondaLinux.py
@@ -0,0 +1,23 @@
+Instalar Conda
+Descarla el instalador de : https://www.anaconda.com/download/#linux
+la version 3.6
+
+Seguir los pasos de este pideo... justa hasta que dice:
+Thanks for installing anaconda, el resto no es necesario
+
+https://www.youtube.com/watch?v=ENlX90p1hDk
+
+
+Luego creas un ambiente Virtual:
+conda create -n iaEnv python=3.6
+conda env list
+source activate iaEnv
+jupyter notebook // Alli puedes crear una carpeta q aloje todos tus notebooks
+source desactivate // Para desactivar el ambiente
+
+Si ya creaste el ambiente, solo ábrelo:
+
+conda env list
+source activate iaEnv
+jupyter notebook // Alli puedes crear una carpeta q aloje todos tus notebooks
+source desactivate // Para desactivar el ambiente
