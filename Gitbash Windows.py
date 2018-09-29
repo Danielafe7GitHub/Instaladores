14bf5f9d1a180f60e775f8b794a0331aaa6e22b9
Danielafe7GitHub <daniela.milon@ucsp.edu.pe>
2018-09-29 10:36:05 -0500

    Create Gitbash Windows.py

---
+++ Gitbash Windows.py
@@ -0,0 +1,11 @@
+#Download from here:
+https://gitforwindows.org/
+#Next to all, not modified anything
+#Configuration
+
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+
+git add .
+git commit -m ""
+git $ git remote add origin https://github.com/XXX
