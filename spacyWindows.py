90d12db725522caf75b9f938386073eadbef72b1
Danielafe7GitHub <daniela.milon@ucsp.edu.pe>
2019-02-16 17:39:13 -0500

    Create spacyWindows.py

---
+++ spacyWindows.py
@@ -0,0 +1,14 @@
+Crear un ambiente virtual en conda 
+#En mi caso fue en u ambiente ya con NLTK
+conda install tqdm
+conda install -c conda-forge spacy /conda install spacy
+#Este paso debe hacer con permiso de administrador 
+#Con cmder :
+#1: Win + Alt + p y allí seleccionas run as administrator
+#2 abres otro tipo de consola : Shift + Alt + 1
+python -m spacy download en
+#walá!!! is done.
+
+Fuentes:
+https://www.youtube.com/watch?v=cgwDB1THUBY&list=PLJ39kWiJXSiz1LK8d_fyxb7FTn4mBYOsD
+http://cmder.net/
