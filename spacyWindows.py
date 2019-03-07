3cb97010233ed1b8b01ef340e46560d980ab8d03
Danielafe7GitHub <daniela.milon@ucsp.edu.pe>
2019-03-07 16:34:55 -0500

    Update spacyWindows.py

--- spacyWindows.py
+++ spacyWindows.py
@@ -6,10 +6,11 @@ conda install -c spacy spacy
 #Este paso debe hacer con permiso de administrador 
 #Con cmder :
 #1: Win + Alt + p y allí seleccionas run as administrator
-#2 abres otro tipo de consola : Shift + Alt + 1
-python -m spacy download en
 #HAY QUE volver a cargar el ambiente en la nueva terminal
 conda activate Nombre 
+#2 abres otro tipo de consola : Shift + Alt + 1
+python -m spacy download en
+
 #walá!!! is done.
 
 Fuentes:
